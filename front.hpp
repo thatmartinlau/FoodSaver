badd282817873baa2dc922a7b34b975cbf31c9cc
CyriacGregoire <147696740+CyriacGregoire@users.noreply.github.com>
2023-11-30 09:04:54 +0100

    Update front.hpp syntax erros fixed

--- front.hpp
+++ front.hpp
@@ -1,25 +1,71 @@
+#ifndef FRONT_HPP
+#define FRONT_HPP
 // Front end class definitions
 #include <iostream>
+#include <list>
 #include <string>
 
-Class User {
-    public:
-    User{};
-    ~User{};
 
-    private:
+enum Food_class{
+    fruit,
+    vegetable,
+    drink,
+    dairy,
+    canned,
+    meat,
+    fish,
+    sweet,
+    nut,
+    other
+};
+
+enum Priority{
+    red,
+    orange,
+    green
+};
+
+class Ingredient{
+public:
+    Ingredient(){};
+    ~Ingredient();
+    void set_FoodClass(){};
+    Food_class get_FoodClass(){};
+
+private:
+    Food_class category;
+    int quantity;
+    std::string expiry_date;
+    Priority priority_level;
+};
+
+class Fridge {
+public:
+    Fridge(){};
+    ~Fridge(){};
+
+private:
+    std::list<Ingredient> ingredient_list;
+};
+
+class User{
+public:
+    User(){};
+    ~User(){};
+
+private:
     std::string username;
     std::string password;
     std::string telegram_username;
     Fridge user_fridge;
 };
 
-Class Offer {
-    public:
-    Offer{};
-    ~Offer{};
+class Offer {
+public:
+    Offer(){};
+    ~Offer(){};
 
-    private:
+private:
     std::list<Ingredient> ingredient_list;
     double price;
     User username;
@@ -28,42 +74,4 @@ Class Offer {
     // add filters in case don't want to see meat and other products
 };
 
-Class Fridge {
-    public:
-    Fridge{};
-    ~Fridge{};
-
-    private:
-    std::list<Ingredient> ingredient_list;
-}
-
-enum Food_class{
-    fruit;
-    vegetable;
-    drink;
-    dairy;
-    canned;
-    meat;
-    fish;
-    sweet;
-    nut;
-    other;
-}
-
-enum Priority{
-    red;
-    orange;
-    green;
-}
-
-Class Ingredient{
-    public:
-    Ingredient{}
-    ~Ingredient{}
-
-    private:
-    Food_class category;
-    int quantity;
-    std::string expiry_date;
-    Priority_level priority;
-}
+#endif // FRONT_HPP
