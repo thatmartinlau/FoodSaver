557fe290489f433e989f7b4490225b59ca41c278
CyriacGregoire <c.gregoire@hotmail.de>
2023-11-30 09:36:03 +0100

    new cpp

---
+++ test3/front.cpp
@@ -0,0 +1,13 @@
+#include "front.hpp"
+#include <iostream>
+#include <list>
+using namespace std;
+
+Ingredient::Ingredient(){
+    this->category = unspecified;
+    this->quantity = 1;
+}
+
+Ingredient::Ingredient(int quantity, Food_class category, string expiry date){
+    this.
+}
