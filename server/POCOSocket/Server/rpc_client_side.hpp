dac95da4a197dcc6e2938c97b89a449ddcd26ea6
adamneveux <adam.neveux@polytechnique.edu>
2023-12-11 14:33:00 +0100

    UPDATEAD hoepfully fixed now

--- server/POCOSocket/Server/rpc_client_side.hpp
+++ server/POCOSocket/Server/rpc_client_side.hpp
@@ -6,30 +6,24 @@
 //Backend guys make a new user, which asks for functions to be called from serverside! All functions here are focused on getting server-coded functions called.
 //Client connection:
 
-class User {
+class ServerUser {
                
 public:
     
 //Creates a user for temporary Use.
-    User(std::string username, std::string psswd);
-    
-    ~User();
+    ServerUser(std::string username, std::string psswd);
+    ~ServerUser();
 
 //createClient will add user data, login, to the server. Checks it isnt already in use, makes hash to give user so he can only access his own data.
 //login details given as a list of 2 strings. 1 means user added, 0 means user not added.
     bool addNewUser();
-
-    
-//Then, we open a session. This checks login details, if valid, opens a session with specific access.
     bool openSession();
-
-//and to close a session
     bool closeSession();
 
 //Client sending specific data types. Template function, we can implement differently for each file type- store food in different place to full fridge upload etc.
 //Make sure to add a category in json file which says YO my hash num is this.
-    template<typename File_Type>
-    bool upload_file(File_Type file);
+    template<typename File_Type> 
+    void addItem(File_Type file);
 
 
 //Client asking for data to be received, chooses which type !!Types to be decided still.
@@ -40,6 +34,15 @@ public:
     bool delete_file(std::string file_name);
 
 private:
-    std::vector<std::string> *login_details;
-
+    std::string username;
+    std::string password;
+    rpc::client client;
 };
+
+
+template<typename JSON_Type>
+void addItem(JSON_Type file) {
+    client.call("addItem", username, password, file);
+}
+
+
