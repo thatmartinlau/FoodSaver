23dee74874083e6d416fbbdeb348fada3bacd37d
adamneveux <adam.neveux@polytechnique.edu>
2023-12-11 15:02:00 +0100

    fixed client-functions.

--- server/POCOSocket/Server/rpc_client_side.hpp
+++ server/POCOSocket/Server/rpc_client_side.hpp
@@ -16,34 +16,16 @@ public:
 
 //createClient will add user data, login, to the server. Checks it isnt already in use, makes hash to give user so he can only access his own data.
 //login details given as a list of 2 strings. 1 means user added, 0 means user not added.
-    bool addNewUser();
-    bool openSession();
-    bool closeSession();
-
-//Client sending specific data types. Template function, we can implement differently for each file type- store food in different place to full fridge upload etc.
-//Make sure to add a category in json file which says YO my hash num is this.
-    template<typename File_Type> 
-    void addItem(File_Type file);
-
-
-//Client asking for data to be received, chooses which type !!Types to be decided still.
-    template <typename File_Type>
-    File_Type receive_file(std::string file_name);
-    
-    template <typename File_Type>
-    bool delete_file(std::string file_name);
+    void add_user();
+    void remove_self();
+    void add_food();
+    void remove_food();
+    void share_food();
+    void get_recommendation();
+    void get_fridge();
 
 private:
     std::string username;
     std::string password;
     rpc::client client;
 };
-
-
-template<typename JSON_Type>
-void addItem(JSON_Type file) {
-    client.call("addItem", username, password, file);
-}
-
-
-heččo89'
