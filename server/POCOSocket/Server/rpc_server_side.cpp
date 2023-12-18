c3f411927aa6375c67a199050c3076efb0ad4648
esma-masic <147949131+esma-masic@users.noreply.github.com>
2023-12-18 14:51:03 +0100

    DB Manipulation functions
    -wrote the basic version of the functions
    -along with completing the task, each function checks if the entry data is valid (namely the username and password)

--- server/POCOSocket/Server/rpc_server_side.cpp
+++ server/POCOSocket/Server/rpc_server_side.cpp
@@ -17,6 +17,9 @@ using namespace std;
 
 class UserData {
 public:
+    UserData(string password){
+        this->password = password;
+    };
     string password;
     vector<vector<string>> fridge;
     vector<vector<vector<vector<string>>>> offer_list;
@@ -33,16 +36,85 @@ void read_from_csv() {
 void save_to_csv() {} //save data of the map in a csv
 
 //DB Manipulation functions:
-void add_user(string username, string password){} //adds user only if user not in already, needs to check for that here/
-void remove_user(string username, string password){}
-void update_user(string old_username, string old_password, string new_username, string new_password) {} //changes a user's name and password. Old is for login verification, new is to change.
+void add_user(string username, string password){
+    if (database->find(username) == database->end()) {
+        // Element doesn't exist, so add it
+        UserData data(password);
+        (*database)[username] = data;
+    }
+}
 
-void update_fridge(std::string username, vector<vector<string>> new_fridge) {} //updates a user fridge
-void update_offer(std::string username, vector<vector<vector<string>>> new_offer) {} //updates a user offer list
+void remove_user(string username, string password){
+    auto el = database->find(username); // Find the username in the database
+    if (el != database->end()) {
+        // Username exists
+        if (password == el->second.password) {
+            // Password matches
+            database->erase(el); // Remove the user if the password matches
+        } else {
+            // Password does not match
+        }
+    } else {
+        // Username not found
+    }
+
+}
+void update_user(string old_username, string old_password, string new_username, string new_password) {
+    auto el = database->find(old_username); // Find the username in the database
+    if (el != database->end()) {
+        // Username exists
+        if (old_password == el->second.password) {
+            // Password matches
+            UserData data = el->second;
+            database->erase(el);
+            add_user(new_username, new_password);
+            }
+        }
+    }//changes a user's name and password. Old is for login verification, new is to change.
+
+void update_fridge(std::string username, string password, vector<vector<string>> new_fridge) {
+        auto el = database->find(username); // Find the username in the database
+        if (el != database->end()) {
+            // Username exists
+            if (password == el->second.password) {
+                // Password matches
+            el->second.fridge = new_fridge;
+            }
+        }
+} //updates a user fridge
+
+void update_offer(std::string username, string password, vector<vector<vector<vector<string>>>> new_offer) {
+        auto el = database->find(username); // Find the username in the database
+        if (el != database->end()) {
+            // Username exists
+            if (password == el->second.password) {
+            // Password matches
+            el->second.offer_list = new_offer;
+            }
+        }
+} //updates a user offer list
 
 //DB Sending Functions:
-vector<vector<vector<string>>> get_offer_list(string username, string password) {}
-vector<vector<string>> get_fridge(string username, string password) {}
+vector<vector<vector<vector<string>>>> get_offer_list(string username, string password) {
+        auto el = database->find(username); // Find the username in the database
+        if (el != database->end()) {
+            // Username exists
+            if (password == el->second.password) {
+            // Password matches
+            return el->second.offer_list;
+            }
+        }
+}
+vector<vector<string>> get_fridge(string username, string password) {
+        auto el = database->find(username); // Find the username in the database
+        if (el != database->end()) {
+            // Username exists
+            if (password == el->second.password) {
+            // Password matches
+            return el->second.fridge;
+            }
+        }
+}
 
 int main() {
     rpc::server srv(3333);
