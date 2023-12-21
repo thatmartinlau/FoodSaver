5a5c66e561bb85d97e1742494d89e04d54065f18
Martin Lau <martinalexlau@gmail.com>
2023-12-21 10:48:05 +0100

    Renaming folders

--- app/app-build (FoodSaver)/FoodSaver - app build/LoginPage.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick 2.12
-import QtQuick.Controls 2.12
-
-Item {
-    Button {
-        text: "Login"
-        onClicked: stackView.push("HomePage.qml")
-    }
-}
--- app/app-build (FoodSaver)/app-build/main.cpp
+++ app/app-build (FoodSaver)/app-build/main.cpp
@@ -1,18 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "Backend.h"
-#include <QQmlContext>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-
-
     QQmlApplicationEngine engine;
-
-    Backend backend;
-    engine.rootContext()->setContextProperty("backend", &backend);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
