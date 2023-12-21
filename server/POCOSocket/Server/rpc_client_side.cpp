3928880a54643c057b8cc8c57e0b9ebff5fd8fac
oscar.peyron@gmail.com <oscar.peyron@polytechnique.edu>
2023-12-21 10:07:02 +0100

    new_version

--- server/POCOSocket/Server/rpc_client_side.cpp
+++ server/POCOSocket/Server/rpc_client_side.cpp
@@ -1,51 +1,174 @@
 #include "rpc/client.h"
 #include <string>
-#include <vector>
-#include <list>
 #include "rpc_client_side.hpp"
+#include<vector>
+#include "../../../test3/front.hpp"
 
 using namespace std;
+
+string HOST_SERVER_NAME = "127.0.0.1"; //change to Server later.
+int HOST_SERVER_PORT = 3333;
 
 ServerUser::ServerUser(string usrname, string psswd) { //Add user to db if user not in db already. Oth, just initialise connection.
     username = usrname;
     password = psswd;
-    rpc::client open_new(HOST_SERVER_NAME, HOST_SERVER_PORT);    
-    open_new.call("add_user", username, password);
+    rpc::client new_cli(HOST_SERVER_NAME, HOST_SERVER_PORT);
+    new_cli.call("add_user", username, password);
 }
+
 
 ServerUser::~ServerUser() {}
 
 
 void ServerUser::delete_self_in_db() {
-    client.call("remove_user", username, password);
+    rpc::client new_cli(HOST_SERVER_NAME, HOST_SERVER_PORT);
+    new_cli.call("remove_user", username, password);
 }
 
 
 void ServerUser::update_user(string new_username, string new_password) {
-    client.call("update_user", username, password, new_username, new_password);
+    rpc::client new_cli(HOST_SERVER_NAME, HOST_SERVER_PORT);
+    new_cli.call("update_user", username, password, new_username, new_password);
 }
 
 
+
+
+Food_class mapCategoryToEnum(const std::string& category) {
+    static const std::unordered_map<std::string, Food_class> categoryMap = {
+        {"fruit", fruit},
+        {"vegetable", vegetable},
+        {"drink", drink},
+        {"dairy", dairy},
+        {"canned", canned},
+        {"meat", meat},
+        {"fish", fish},
+        {"sweet", sweet},
+        {"nut", nut},
+        {"unspecified", unspecified},
+        {"other", other}
+    };
+
+    auto it = categoryMap.find(category);
+    if (it != categoryMap.end()) {
+        return it->second;
+    } else {
+        return unspecified;
+    }
+}
+
+Priority mapprioToEnum(const std::string& priority_level) {
+    static const std::unordered_map<std::string, Priority> PrioMap = {
+        {"red", red},
+        {"orange",orange},
+        {"green",green}
+    };
+
+    auto it = PrioMap.find(priority_level);
+    if (it != PrioMap.end()) {
+        return it->second;
+    }
+}
+
+
+std::string_view foodClassToString(Food_class foodClass) {
+    static const std::unordered_map<Food_class, std::string_view> FoodClassToString = {
+        {Food_class::fruit, "fruit"},
+        {Food_class::vegetable, "vegetable"},
+        {Food_class::drink, "drink"},
+        {Food_class::dairy, "dairy"},
+        {Food_class::canned, "canned"},
+        {Food_class::meat, "meat"},
+        {Food_class::fish, "fish"},
+        {Food_class::sweet, "sweet"},
+        {Food_class::nut, "nut"},
+        {Food_class::unspecified, "unspecified"},
+        {Food_class::other, "other"}
+    };
+
+    auto it = FoodClassToString.find(foodClass);
+    if (it != FoodClassToString.end()) {
+        return it->second;
+    } else {
+        return "Unknown";
+    }
+}
+
+
+
+
+
+std::string priorityToString(Priority priority) {
+    static const std::unordered_map<Priority, std::string> PriorityToString = {
+        {Priority::red, "red"},
+        {Priority::orange, "orange"},
+        {Priority::green, "green"}
+    };
+
+    auto it = PriorityToString.find(priority);
+    if (it != PriorityToString.end()) {
+        return it->second;
+    } else {
+        return "Unknown"; // Return a default string for unknown values
+    }
+}
+
 //OSCAR's CODE ADAPTED BY ADAM: Oscar, feel free to adjust and change if u have a way which matches smth u prefer to use.
-vector ingredient_to_vector(Ingredient &ingr) { //returns vector as [name, quantity, category, expiry_date, priority_level], all as string s.
-    vector vec;
-    vec.push_back(ingr.name);
-    vec.push_back(ingr.quantity);
-    string cat_string = nameof(ingr.category); //gives the enum to string format.
+vector<string> ingredient_to_vector(Ingredient &ingr) { //returns vector as [name, quantity, category, expiry_date, priority_level], all as string s.
+    vector<string> vec;
+    vec.push_back(ingr.get_name());
+    vec.push_back(to_string(ingr.get_Quantity()));
+    std::string cat_string(foodClassToString(ingr.get_FoodClass())); // Convert std::string_view to std::string
     vec.push_back(cat_string);
-    vec.push_back(ingr.expiry_date);
+    vec.push_back(ingr.get_expiry_date()); // need Sixtine's get_expiry date
     string priority_level_string = nameof(ingr.priority_level);
     vec.push_back(ingr.priority_level);
     return vec;
 }
 
-Ingredient ingredient_from_vector(vector) { //Oscar work yo magic
-    
+
+
+
+
+
+Ingredient ingredient_from_vector(std::vector<std::string> vector) { //Oscar work yo magic
+ // Convert string name into --> string name --> nothing to change
+    std::string name = vector[0];
+
+    // Convert string quantity into --> int quantity :
+    int quantity = std::stoi(vector[1]);
+
+    // Convert string category into --> in food class enum
+
+    Food_class category = mapCategoryToEnum(vector[2]);
+
+    // Convert string expiry_date --> string --> nothing to change
+
+    std::string expiry_date =vector[3];
+
+    // Convert priority level string into --> enum
+
+    Priority priority_level = mapprioToEnum(vector[4]);
+
+
+    Ingredient ingredient = Ingredient(name,category,expiry_date);
+    ingredient.set_priority(priority_level);
+    return ingredient;
 }
 
-Fridge ServerUser::get_fridge() {
-    vector<vector<string>> fridge_vector = client.call("get_fridge", username, password); //vector of ingredients, each as a vector of strings.
+Fridge ServerUser::get_fridge() { // [[]]
+    rpc::client new_cli(HOST_SERVER_NAME, HOST_SERVER_PORT);
+    std::vector<vector<string>> fridge_vector = new_cli.call("get_fridge", username, password); //vector of ingredients, each as a vector of strings.
     //Oscar work yo magic
+    //     Fridge(std::list<Ingredient> init_list);
+    std::vector<Ingredient> vector_Ingredient;
+
+    for (size_t i = 0; i < fridge_vector.size(); ++i) {
+        vector_Ingredient.push_back(ingredient_from_vector(fridge_vector[i]));
+    }
+
+    return Fridge(vector_Ingredient);
+
 }
 
 
@@ -59,23 +182,87 @@ void ServerUser::update_fridge(Fridge &f_input) {
         fridge.push_back(ingredient_to_vector(ingredient));
     }
     //send as new format
-    client.call("update_fridge", username, password, fridge_vector);
+    rpc::client new_cli(HOST_SERVER_NAME, HOST_SERVER_PORT);
+    new_cli.call("update_fridge", username, password, fridge_vector);
 }
 
 
 
-list<Offer> ServerUser::get_offer_list() {
-    vector<vector<vector<string>>> offer_list_vector = client.call("get_offer_list", username, password);
+vector<Offer> ServerUser::get_offer_list() {
+    rpc::client new_cli(HOST_SERVER_NAME, HOST_SERVER_PORT);
+    vector<vector<vector<vector<string>>>> offer_list_vector = new_cli.call("get_offer_list", username, password);
     //Oscar work yo magic: ADDED DETAIL: The offer list format holds offers, each other has this format exactly: [vector<string>, vector<string>], Where the second vector stores only a double!
     //That double represents the price of the offer. We needed this because vectors have homogenous type. (only store one type.)
     
+    //    double doubleValue = std::stod(numericalString);
+    std::vector<Offer> vector_offer;
+
+
+    for (size_t i = 0; i < offer_list_vector.size(); ++i) {
+        double price = std::stod(offer_list_vector[i][1][0][0]);
+        // offer_list[i][0] convert into a vector of ingredient .
+
+        std::vector<Ingredient> vector_offer_list;
+
+        for (size_t j = 0; j < offer_list[i][0].size(); ++i) {
+            vector_offer_list.push_back(ingredient_from_vector(offer_list[i][0][j]));
+        }
+
+        Offer offer = Offer(vector_offer_list);
+        offer.set_price(price);
+        vector_offer.push_back(offer);
+    return vector_offer;
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
 }
 
-void ServerUser::update_offer_list(list<Offer> &offer_list) {
+
+
+void ServerUser::update_offer_list(vector<Offer> &offer_list) {
     //Oscar work yo magiiiic: same format as get_offer_list for the data we want to give to the server.
-    
-    
-    client.call("update_offer_list", username, password, new_offer_list); //this in the weird new vector<vector<vector<string>>> format, sorry! Check telegram paper image n1 for clarity, hopefully :)
+
+    // vector<Offer> -->
+
+
+
+    // [ [ [ [name1, quantity1, category1, expiry_date1, priority_level1], [name2, quantity2, category2, expiry_date2, priority_level2], ...], [[price]] ],  .... ,[ [ [name1', quantity1', category1', expiry_date1', priority_level1'], [name2', quantity2', category2', expiry_date2', priority_level2'], ...], [[price']] ]
+    // [[Ingredient list1, price1] , [Ingredient list 2, price2] ,...]
+    vector<vector<vector<vector<string>>>> update_offer;
+    for (size_t j = 0; j < offer_list.size(); ++j){
+        Offer offer_elem =  offer_list[j];
+        double price = offer_elem.get_price();
+        string string_price = to_string(price);
+
+
+
+
+
+
+
+
+
+    }
+    rpc::client new_cli(HOST_SERVER_NAME, HOST_SERVER_PORT);
+    new_cli.call("update_offer_list", username, password, new_offer_list); //this in the weird new vector<vector<vector<vector<string>>>> format, sorry! Check telegram paper image n1 for clarity, hopefully :)
+
+
+
+
+
+
+
 }
 
 
--- server/POCOSocket/Server/rpc_client_side.hpp
+++ server/POCOSocket/Server/rpc_client_side.hpp
@@ -1,38 +1,29 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <list>
 #include "rpclib-master/include/rpc/client.h"
 #include "../../../test3/front.hpp"
 
-using namespace std;
 
 //Backend guys make a new user, which asks for functions to be called from serverside! All functions here are focused on getting server-coded functions called.
 //Client connection:
 //The class User from front.cpp would have a private variable serverUser belonging to this class.
 //On every call which requests or requires, or gives data to the server database, simply call the sending/receiving functions given down here.
 
-
-const string HOST_SERVER_NAME = "127.0.0.1"; //change to Server later.
-const int HOST_SERVER_PORT = 3333;
-
-
 class ServerUser {
                
 public:
     
 //Creates a user for temporary Use.
-    ServerUser(string username, string psswd);//if user doesn't exist in database, add him to database.
+    ServerUser(std::string username, std::string psswd);//if user doesn't exist in database, add him to database.
     ~ServerUser();
 
     void delete_self_in_db(); //deletes the user from the database.
     
     Fridge get_fridge(); //receives the fridge from the database.
     void update_fridge(); //updates the fridge on the database. Adds/removes necessary items with comparisons. Keeps the local fridge unchanged.
-    list<Offer> get_offer_list(); //gets db's offer list for user
+    std::list<Offer> get_offer_list(); //gets db's offer list for user
     void update_offer_list(); //updates db's offer list.
-    void update_user(string new_username, string new_password); //gives new username and password to database.
+    void update_user(std::string new_username, std::string new_password); //gives new username and password to database.
 private:
-    string username;
-    string password;
+    std::string username;
+    std::string password;
 };
