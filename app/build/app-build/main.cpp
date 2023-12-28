18b00f9abad48c84b330ab3697c691e8d2461179
CyriacGregoire <147696740+CyriacGregoire@users.noreply.github.com>
2023-12-28 17:45:11 +0100

    Update connection to Maincontroller

--- app/build/app-build/main.cpp
+++ app/build/app-build/main.cpp
@@ -1,11 +1,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "maincontroller.h"
+
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/QML/main.qml")));
+
+    MainController mainController;
+    engine.rootContext()->setContextProperty("MainController", &mainController);
+
+
+    engine.addImportPath(QCoreApplication::applicationDirPath() + "/qml");
+    engine.addImportPath(":/");
+
 
     return app.exec();
 }
