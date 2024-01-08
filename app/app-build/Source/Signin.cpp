985ddd1b2f9e0841001e037cdeb6285a983ea673
Fannyscherer <fanny.scherer@polytechnique.edu>
2024-01-08 14:08:50 +0100

    Update Signin.cpp

--- app/app-build/Source/Signin.cpp
+++ app/app-build/Source/Signin.cpp
@@ -1,15 +1,18 @@
 #include "../Header/Signin.h"
+#include "../Header/user.h"
 Signin::Signin(QObject *parent) : QObject(parent) {}
 
-
-bool Signin::signIn(const QString &username, const QString &password, const QString &password_check) {
+//Add telegram
+bool Signin::signIn(const QString &username, const QString &password, const QString &password_check, const QString &telegram) {
     //STILL NEED TO ADD IN THE VERIFICATION FOR EXISTING USERNAME
     if (username== "") {
         emit openNoUsernameError();
         return false;
     }
-    else if (username!= "Fanny") {
+    else if {
         emit openUsernameError(); //show username error
+        User new_user;
+        new_user.set_username(username);
         return false;
     }
     else if (password ==""||password_check=="") {
@@ -21,6 +24,7 @@ bool Signin::signIn(const QString &username, const QString &password, const QStr
         return false;
     }
     else {
+        User new_user(username, password, telegram);
         emit openMarketPage(); //open Market.qml
         return true;
     }
