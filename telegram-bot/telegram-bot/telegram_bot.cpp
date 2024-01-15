fbe5cf169e73278d36e677a0976bf9abd8922e67
Ziyue_Qiu_William <qiuzy.william2019@gmail.com>
2024-01-15 19:42:28 +0100

    Update telegram_bot.cpp

--- telegram-bot/telegram-bot/telegram_bot.cpp
+++ telegram-bot/telegram-bot/telegram_bot.cpp
@@ -20,16 +20,20 @@ struct UserInfo {
     std::string password;
 };
 
+struct UserCredentials {
+    std::string username;
+    std::string password;
+};
+
+std::map<int64_t, UserCredentials> userCredentialsMap;
+
 std::map<int64_t, UserInfo> userStates;
 
 
-void link_verification(client client, string token) {}
-
-void send_remind_messages(client client) {}
-
 string check_expiration(vector<vector<string>> fridge) {}
 
-string check_fridge(client)
+void link_verification(client client, string token) {}
+
 
 int main() {
     TgBot::Bot bot("6644281748:AAFh40LQLa5054caEUPt8T_9wf-Yv1hAB-w");
@@ -37,24 +41,22 @@ int main() {
     rpc::client c("3333", rpc::constants::DEFAULT_PORT);
 
     bot.getEvents().onCommand("Start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Welcome to FoodSaver, I can help you manage your fridge in Apps." 
+        bot.getApi().sendMessage(message->chat->id, "Welcome to FoodSaver bot, I can help you manage your fridge account." 
                                                     "Here are the things that I can do for you:\n"
+                                                    "- /Login - login to your fridge account\n"
                                                     "- /Check_your_fridge - check your fridge contents\n"
-                                                    "- /Link_your_fridge - link your fridge account to this bot\n"
-                                                    "- /add_food - add food to your fridge\n"
-                                                    "- /remove_food - remove food from your fridge";);
+                                                    "- /update_fridge - update the entire fridge";);
     });
 
     //
     bot.getEvents().onCommand("check_fridge", [&bot, &userStates](TgBot::Message::Ptr message) {
-    UserInfo& userInfo = userStates[message->chat->id];
-    userInfo.state = UserState::AwaitingUsername;
-    bot.getApi().sendMessage(message->chat->id, "Enter your username:");
+        UserInfo& userInfo = userStates[message->chat->id];
+        userInfo.state = UserState::AwaitingUsername;
+        bot.getApi().sendMessage(message->chat->id, "Enter your username:");
     });
 
     bot.getEvents().onAnyMessage([&bot, &userStates, &c](TgBot::Message::Ptr message) {
         if (StringTools::startsWith(message->text, "/")) {
-            // Ignore command messages
             return;
         }
 
@@ -70,47 +72,56 @@ int main() {
             userInfo.state = UserState::None;
 
             // Here you call your RPC function with the collected username and password
-            double fridge = c.call("get_fridge", userInfo.username, userInfo.password).as<double>();
+            string fridge = c.call("get_fridge", userInfo.username, userInfo.password).as<double>();
 
-            bot.getApi().sendMessage(chatId, "Your fridge contains: " + std::to_string(fridge));
+            bot.getApi().sendMessage(chatId, "Your fridge contains: " + fridge);
         }
     });
 
 
     //
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-    if (StringTools::startsWith(message->text, "/link_your_fridge")) {
-        return;
-    }
-
-    bot.getApi().sendMessage(message->chat->id, "Enter your username: " + message->text);
-
-    // Codes for receive username from user, username
-
-    bot.getApi().sendMessage(message->chat->id, "Enter your password: " + message->text);
-
-    // Codes for receive password from user, password
-
-    double server_message = c.call("get_token", username, password).as<double>();
-
-    if (server_message == "1") {
-        bot.getApi().sendMessage(message->chat->id, "Linked successfully!" + message->text);
-    } 
-    else if (server_message == "0") {
-        bot.getApi().sendMessage(message->chat->id, "This account has already been linked in telegram." + message->text);
-    }
+    bot.getEvents().onCommand("login", [&bot, &userStates](TgBot::Message::Ptr message) {
+        int64_t chatId = message->chat->id;
+        if (userCredentialsMap.find(chatId) != userCredentialsMap.end()) {
+            // User already logged in
+            bot.getApi().sendMessage(chatId, "This telegram account already login.");
+        } else {
+            // User needs to log in
+            UserInfo& userInfo = userStates[message->chat->id];
+            userInfo.state = UserState::AwaitingUsername;
+            bot.getApi().sendMessage(chatId, "Enter your password: ");
+        }
     });
 
-    //
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-    if (StringTools::startsWith(message->text, "/add_food")) {
-        return;
-    }
+    bot.getEvents().onAnyMessage([&bot, &userStates, &c](TgBot::Message::Ptr message) {
+        if (StringTools::startsWith(message->text, "/")) {
+            return;
+        }
 
-    bot.getApi().sendMessage(message->chat->id, "Enter the food you want to add: " + message->text);
+        int64_t chatId = message->chat->id;
+        UserInfo& userInfo = userStates[chatId];
+
+        if (userInfo.state == UserState::AwaitingUsername) {
+            userInfo.username = message->text;
+            userInfo.state = UserState::AwaitingPassword;
+            bot.getApi().sendMessage(chatId, "Enter your password:");
+        } else if (userInfo.state == UserState::AwaitingPassword) {
+            userInfo.password = message->text;
+            userInfo.state = UserState::None;
+
+            // RPC call to link the fridge
+            double server_message = c.call("get_token", userInfo.username, userInfo.password).as<double>();
+
+            // Check the server's response
+            if (server_message == 1) {
+                bot.getApi().sendMessage(chatId, "Linked successfully!");
+            } else if (server_message == 0) {
+                bot.getApi().sendMessage(chatId, "This account has already been linked in Telegram.");
+            } else {
+                bot.getApi().sendMessage(chatId, "An error occurred while linking your fridge.");
+            }
+        }
     });
-
-    // receive message from user
 
     try {
         printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
