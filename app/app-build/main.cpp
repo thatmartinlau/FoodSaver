118c462949a23cdc1720a94416239bb8c8182b77
CyriacGregoire <c.gregoire@hotmail.de>
2024-01-26 22:37:16 +0100

    change - opens a single window instead of two

--- app/app-build/main.cpp
+++ app/app-build/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]) {
     engine.addImportPath(QCoreApplication::applicationDirPath() + "/qml");
     engine.addImportPath(":/");
 
-    engine.load(QUrl(QStringLiteral("qrc:/QML/main.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/QML/main.qml")));
 
     engine.load(QUrl(QStringLiteral("qrc:/QML/main.qml")));
 
